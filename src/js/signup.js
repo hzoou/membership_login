0262af6ed9be2c4e9b7fb1a93eab71692144ee92
hzoou <woojooo52@naver.com>
2019-08-29 18:27:26 +0900

    비밀번호 재확인 validation

--- src/js/signup.js
+++ src/js/signup.js
@@ -16,6 +16,24 @@ const check = {
             text.innerText = '5~20자의 영문 소문자, 숫자와 특수기호(_)(-) 만 사용 가능합니다.';
             text.style.color = color.x;
         }
+    },
+
+    pwSame() {
+        const text = document.getElementById('pw2Txt');
+        if (pw.value === pw2.value) {
+            validation = true;
+            text.innerText = '비밀번호가 일치합니다.';
+            text.style.color = color.o;
+        } else {
+            validation = false;
+            text.innerText = '비밀번호가 일치하지 않습니다.';
+            text.style.color = color.x;
+        }
+
+        if (pw.value == pw2.value && !pw.value) {
+            validation = false;
+            text.innerText = '';
+        }
     }
 };
 
@@ -28,5 +46,9 @@ months.forEach((m) => {
 
 let validation = false;
 const id = document.getElementById('id');
+const pw = document.getElementById('pw');
+const pw2 = document.getElementById('pw2');
 
 id.addEventListener("blur", check.idReg);
+pw.addEventListener("change", check.pwSame);
+pw2.addEventListener("blur", check.pwSame);
