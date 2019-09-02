80228d33af94cc90d083b461e338a0c45b21895d
hzoou <woojooo52@naver.com>
2019-09-02 15:15:48 +0900

    signin 페이지 html

---
+++ src/js/html/signinHtml.js
@@ -0,0 +1,12 @@
+<div>
+    <div id="title">로그인</div>
+    <div class="inputText">
+        <input type="text" id="id" placeholder="아이디"/>
+    </div>
+    <span id="idTxt"></span>
+    <div class="inputText">
+        <input type="password" id="pw" placeholder="비밀번호"/>
+    </div>
+    <span id="pwTxt"></span>
+    <input type="submit" value="로그인"/>
+</div>
