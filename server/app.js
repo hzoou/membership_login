c0b70b79ae9a07d36651ef5dafec0684caa1fb00
hzoou <woojooo52@naver.com>
2019-09-05 23:38:56 +0900

    [BE] Add signup api

--- server/app.js
+++ server/app.js
@@ -1,11 +1,8 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
-const indexRouter = require('./routes/index');
-
+const signup = require('./routes/signup');
 const app = express();
 
 // view engine setup
@@ -20,22 +17,6 @@ app.use(cookieParser());
 const dirPath = __dirname;
 app.use(express.static(path.join(dirPath.replace("/server", "/"))));
 
-app.use('/', indexRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use('/signup', signup);
 
 module.exports = app;
