7c49924741ee61a502b56e86a8678fb73d3af4f1
hzoou <woojooo52@naver.com>
2019-09-06 19:29:20 +0900

    [BE] Add using pug, page display after logout

---
+++ server/routes/logout.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { deleteSession } = require("../database/database");
+
+router.get('/', (req, res) => {
+    const sessionId = req.cookies.sessionId;
+    deleteSession(sessionId);
+    res.clearCookie('sessionId').render('index');
+});
+
+module.exports = router;
