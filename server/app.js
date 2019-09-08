fb75adb3c8aeacc5415a013ad99bdca1eb38358e
hzoou <woojooo52@naver.com>
2019-09-08 19:11:30 +0900

    [BE] Add exception handling error

--- server/app.js
+++ server/app.js
@@ -1,3 +1,5 @@
+const createError = require('http-errors');
+const cookieParser = require('cookie-parser');
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
@@ -7,6 +9,7 @@ const mypage = require('./routes/mypage');
 const logout = require('./routes/logout');
 const app = express();
 
+app.use(cookieParser());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -21,5 +24,8 @@ app.use('/logout', logout);
 app.use('/mypage', mypage);
 app.use('/signup', signup);
 app.use('/signin', signin);
+app.use((req, res, next) => {
+    res.redirect(`http://localhost:3000/#${req.url.substr(1, req.url.length)}`);
+});
 
 module.exports = app;
