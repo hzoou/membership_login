cda6273cc8eefdce9bc1e520511910013670e62c
hzoou <woojooo52@naver.com>
2019-09-09 15:42:27 +0900

    [BE] Remove timeout of cookie

--- server/routes/signin.js
+++ server/routes/signin.js
@@ -9,10 +9,12 @@ router.post('/', (req, res) => {
         if (isCorrectPw(req.body.id, req.body.pw)) {
             const sessionId = uuid4();
             createSession(sessionId, req.body.id);
-            res.cookie('sessionId', sessionId, {maxAge: 60 * 30 * 1000}).status(200).send({
-                status: "SUCCESS",
-                msg: "로그인 완료"
-            });
+            res.cookie('sessionId', sessionId)
+                .status(200)
+                .send({
+                    status: "SUCCESS",
+                    msg: "로그인 완료"
+                });
         } else res.status(200).send({status: "FAIL", msg: "비밀번호가 일치하지 않습니다."});
     } else res.status(200).send({status: "FAIL", msg: "아이디가 존재하지 않습니다."});
 });
